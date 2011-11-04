d18f18083866a4ac03896fd3a8d8bc8e78b9ae26
Kresten Krab Thorup <krab@trifork.com>
2011-11-04 13:08:16 +0100

    Add generic equal(o,o) for collections to util

--- src/utils/utils.dart
+++ src/utils/utils.dart
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2011 by Trifork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 int floor(double num) {
   String str = num.toString();
   int idx = str.indexOf(".");
@@ -68,3 +84,48 @@ reduce(coll, init, f) {
     });
   return cur;
 }
+
+
+//
+// Implements equals for collections (defaults to == for others)
+//
+
+equal(o1,o2) {
+
+    if (o1==o2)
+	return true;
+
+    switch(true) {
+    case o1 is Map:
+	return _map_equal(o1,o2);
+
+    case o1 is List:
+	return _list_equal(o1,o2);
+
+    case o1 is Set:
+	return _set_equal(o1, o2);
+
+    default:
+	return o1==o2;
+    }
+}
+
+_map_equal(map1,map2) {
+    if (map1==map2)
+	return true;
+
+    if (map1 is !Map || map2 is !Map)
+	return false;
+
+    if (map1.length != map2.length)
+	return false;
+
+    for(var k in map1.getKeys()) {
+	if (!equal(map1[k], map2[k]))
+	    return false;
+    }
+
+    return true;
+}
+
+
