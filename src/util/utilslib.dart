644bbd7333f1d8e4e96986187c225e2721a906c6
Karl Krukow <karl.krukow@gmail.com>
2011-11-04 14:18:21 +0100

    more dart deps

--- src/util/utilslib.dart
+++ src/util/utilslib.dart
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO(vsm): Don't monkey patch Object and make this work on
-// non-chrome browsers.
-Object.prototype.get$typeName = function() {
-  return this.constructor.name;
-}
+#library('utilslib');
+
+#import('../observable/observable.dart');
+
+#source('CollectionUtils.dart');
+#source('DateUtils.dart');
+#source('StringUtils.dart');
+#source('Uri.dart');
